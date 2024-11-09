c932910d104156a031b266b87cdd5e0bc5d2b93d
LevButarev <butarevlev@gmail.com>
2024-11-09 11:26:22 +0300

    091124
    Create files program cpp

---
+++ set0C/setoC.cpp
@@ -0,0 +1,23 @@
+/*
+setoC
+student - Butarev Lev
+091124
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    for(int a = 0; a <= 4; a += 1)
+    {
+        for (int j = 1; j <= 5; j += 1)
+    {
+        cout << "*";
+    }
+
+    cout << endl;
+
+    }
+    return 0;
+}
