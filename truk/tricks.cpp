8cb8d8110e73965d1c863c8f002a3ebe0c28cb5d
LevButarev <butarevlev@gmail.com>
2024-11-16 10:40:39 +0300

    161124
    Make program tricks cpp

---
+++ truk/tricks.cpp
@@ -0,0 +1,24 @@
+/*
+tricks
+student - Butarev Lev
+091124
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int x = 45;
+    int y = 54;
+
+    int z = (x > y) ? x : y;
+
+    cout << z << endl;
+    return 0;
+}
+
+
+
+
---
+++ truk/triks.cpp
@@ -0,0 +1,56 @@
+/*
+tricks
+student - Butarev Lev
+091124
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Hello World" << endl;
+    
+    
+    return 0;
+}
+
+
+/*
+tricks
+student - Butarev Lev
+091124
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define what cout << "What" << "\n";
+
+
+int main()
+{
+    
+    what;
+    return 0;
+}
+
+
+/*
+tricks
+student - Butarev Lev
+091124
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main()
+{
+    int x = 1e12 + 5;
+    int y = 1e10;
+
+    cout << x + y << endl;
+    return 0;
+}
+
+
