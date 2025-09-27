1d2e7d751d81b61f2692aad8b1e2f44454cecb14
LevButarev <butarevl06@gmail.com>
2025-09-27 12:50:20 +0300

    27.09.25
    c++_style,c_style

--- Text.cpp
+++ Text.cpp
@@ -11,6 +11,15 @@ int main()
     char c[] = {'t','e','x','t','\0'};
 
     string d = "text"; // c++ style
+    string f("text");
 
+    int sizeb0 = sizeof(b[0]);
+    int sizeb = sizeof(b);
+    int count = (sizeb / sizeb0) - 1;
+    cout << count << endl;  // c style
+
+    
+    int lengthb = d.length();
+    cout << lengthb << endl; // c++ style
     return 0;
 }
